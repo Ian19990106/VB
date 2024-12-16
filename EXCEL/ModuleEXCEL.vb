025de154c62aedf06d78ec1bda8bab4f9b770929
Ian19990106 <qwe429854@gmail.com>
2024-12-16 17:40:06 +0800

    Add files via upload

---
+++ EXCEL/ModuleEXCEL.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+Imports Microsoft.Office.Interop.Excel
+
+Module ModuleEXCEL
+    Public Sub ExportToExcel(listView As ListView)
+        ' 初始化 Excel 物件
+        Dim excelApp As New Microsoft.Office.Interop.Excel.Application()
+        Dim workbook As Workbook = excelApp.Workbooks.Add()
+        Dim worksheet As Worksheet = CType(workbook.Sheets(1), Worksheet)
+
+        ' 將 ListView 的列標題寫入 Excel
+        For col As Integer = 0 To listView.Columns.Count - 1
+            worksheet.Cells(1, col + 1) = listView.Columns(col).Text
+        Next
+
+        ' 將 ListView 的資料寫入 Excel
+        For row As Integer = 0 To listView.Items.Count - 1
+            For col As Integer = 0 To listView.Columns.Count - 1
+                worksheet.Cells(row + 2, col + 1) = listView.Items(row).SubItems(col).Text
+            Next
+        Next
+
+        ' 顯示 Excel 並保存檔案
+        excelApp.Visible = False
+
+        ' 選擇是否保存檔案（可選）
+        Dim dataFolderPath As String = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\Excel")
+        Dim savePath As String = IO.Path.Combine(dataFolderPath, "A.xlsx")
+
+        If (Directory.Exists(dataFolderPath) = False) Then
+            Directory.CreateDirectory(dataFolderPath)
+        End If
+
+        workbook.SaveAs(savePath)
+        workbook.Close()
+        excelApp.Quit()
+
+        ' 釋放資源
+        ReleaseObject(worksheet)
+        ReleaseObject(workbook)
+        ReleaseObject(excelApp)
+
+        MessageBox.Show("匯出成功！文件保存在: " & savePath)
+    End Sub
+
+    Private Sub ReleaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+End Module
---
+++ SQL/ModuleSQL.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Data.SqlClient
+
+Module ModuleSQL
+
+    Const connectionString As String = "Data Source=IAN\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False"
+
+    ''' <summary>
+    ''' SQL語法執行
+    ''' </summary>
+    Public Sub ExecuteSQLQuery(query As String)
+        ' 使用 Using 確保資源釋放
+        Using connection As New SqlConnection(connectionString)
+            Try
+                ' 開啟連線
+                connection.Open()
+                Console.WriteLine("連線成功！")
+
+                ' 建立 SQL 指令
+                Using command As New SqlCommand(query, connection)
+                    command.ExecuteReader()
+                End Using
+            Catch ex As Exception
+                ' 處理例外
+                Console.WriteLine($"發生錯誤：{ex.Message}")
+            Finally
+                ' 確保連線關閉
+                If connection.State = ConnectionState.Open Then
+                    connection.Close()
+                End If
+            End Try
+        End Using
+    End Sub
+
+    ''' <summary>
+    ''' 查詢SQL資料回傳DataTable
+    ''' </summary>
+    Public Function GetDataTable(query As String) As System.Data.DataTable
+        Dim dataTable As New System.Data.DataTable()
+
+        Try
+            ' 使用 SqlConnection 連接資料庫
+            Using connection As New SqlConnection(connectionString)
+                connection.Open()
+
+                ' 使用 SqlDataAdapter 執行查詢並填充 DataTable
+                Using adapter As New SqlDataAdapter(query, connection)
+                    adapter.Fill(dataTable)
+                End Using
+            End Using
+        Catch ex As Exception
+            ' 捕捉例外並顯示錯誤訊息
+            Console.WriteLine($"讀取資料時發生錯誤：{ex.Message}")
+        End Try
+
+        Return dataTable
+    End Function
+End Module
